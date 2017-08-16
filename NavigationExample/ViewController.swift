f0a5b0001e4ec680de426199adc9c7e5e52c25af
Thach Nguyen <pcngocthach@gmail.com>
2017-08-16 15:38:01 +0700

    Initial Commit

---
+++ NavigationExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationExample
+//
+//  Created by Thach Nguyen on 8/16/17.
+//  Copyright © 2017 Thach Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
